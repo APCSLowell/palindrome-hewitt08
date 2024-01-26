5d1ec54e569bcd6fb96f5fcfafcd9052c28518d0
hewitt08 <143777083+hewitt08@users.noreply.github.com>
2024-01-26 10:46:32 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,14 +37,16 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
+  if(
   return false;
 }
 public String reverse(String str)
 {
     String sNew = new String();
     for(int i = str.length(); i > 0; i--){
-      sNew = sNew + str.substring(i-1, i);
+      if(Character.isLetter(str.substring(i-1, i))){
+        sNew = sNew + str.substring(i-1, i);
+      }
     }
     return sNew;
 }
